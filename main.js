6652efc5c15058d356d21bd8d9fe773216c77c4d
newgols51-pixel <newgols51@gmail.com>
2026-06-15 16:14:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+// This file was created to host the Sentry verification snippet.
+// Sentry "Verify" test: this intentionally calls an undefined function
+// to confirm error monitoring is capturing exceptions correctly.
+
+myUndefinedFunction();
