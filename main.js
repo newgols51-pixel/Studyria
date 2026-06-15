6ed1d3db2167aa80a8088d51660a7be07d303dc1
newgols51-pixel <newgols51@gmail.com>
2026-06-15 16:22:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,3 @@
 // This file was created to host the Sentry verification snippet.
 // Sentry "Verify" test: this intentionally calls an undefined function
 // to confirm error monitoring is capturing exceptions correctly.
-
-myUndefinedFunction();
